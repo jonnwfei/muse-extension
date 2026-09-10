195ae8de804f42eca3c7403c871677f0ae17b1f2
John Cai <john.cai@student.kuleuven.be>
2026-09-10 20:24:27 +0200

    chore: removed config.ts from ignore and added config

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+// @author John Cai
+// @date 2026-09-09
+
+export const CONFIG = {
+  SHEET_CLASS: "Q05uP",
+  SCALE: 2, // Scale factor for rendering images to canvas (optimal: 2)
+  CANVAS_TO_BLOB_QUALITY: 0.005, // Quality for canvas to Blob conversion (0 to 1, optimal: 0.005)
+  // Lower than 0.005 wont impact the file size
+};
